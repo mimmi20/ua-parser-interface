43e7e2d6041352804855436654228aa0261f74f3
Thomas Müller <mimmi20@live.de>
2025-01-12 13:03:29 +0100

    add new interfaces

---
+++ src/ClientVersionInterface.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * This file is part of the mimmi20/ua-parser-interface package.
+ *
+ * Copyright (c) 2025, Thomas Mueller <mimmi20@live.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+
+namespace UaParser;
+
+interface ClientVersionInterface
+{
+    /** @throws void */
+    public function hasClientVersion(string $value): bool;
+
+    /**
+     * @return non-empty-string | null
+     *
+     * @throws void
+     */
+    public function getClientVersion(string $value, string | null $code = null): string | null;
+}
---
+++ src/PlatformCodeInterface.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * This file is part of the mimmi20/ua-parser-interface package.
+ *
+ * Copyright (c) 2025, Thomas Mueller <mimmi20@live.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+
+namespace UaParser;
+
+interface PlatformCodeInterface
+{
+    /** @throws void */
+    public function hasPlatformCode(string $value): bool;
+
+    /**
+     * @return non-empty-string | null
+     *
+     * @throws void
+     */
+    public function getPlatformCode(string $value, string | null $derivate = null): string | null;
+}
